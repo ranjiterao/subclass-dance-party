db1fe45878459362d64d0b83709cf2a8ff98ef48
ranjiterao <ranjiterao@gmail.com>
2015-11-30 22:25:19 -0800

    made additional changes to two dancers

--- src/dancer.js
+++ src/dancer.js
@@ -13,8 +13,9 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  this.count++;
-  setTimeout(this.step, this.timeBetweenSteps);
+  var that = this;
+  
+  setTimeout(function() { that.step(); }, this.timeBetweenSteps);
 };
 
 
